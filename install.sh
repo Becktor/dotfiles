ad2c81b0773ce60ad867f293e3b97d61d4f379f5
Jonathan Becktor <Jonathan@Becktor.org>
2025-03-04 08:42:50 -0800

    updated neovim and tmux

---
+++ install.sh
@@ -0,0 +1,33 @@
+ #!/bin/bash
+
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Install prerequisites
+echo "Installing prerequisites..."
+sudo apt-get update
+sudo apt-get install -y ninja-build gettext cmake curl build-essential tmux
+
+# Clone Neovim into home directory
+NEOVIM_DIR="$HOME/neovim"
+if [ -d "$NEOVIM_DIR" ]; then
+    echo "Neovim directory already exists. Pulling latest changes."
+    cd "$NEOVIM_DIR" && git pull origin master
+else
+    echo "Cloning Neovim repository..."
+    git clone https://github.com/neovim/neovim.git "$NEOVIM_DIR"
+fi
+
+# Build Neovim
+cd "$NEOVIM_DIR"
+make CMAKE_BUILD_TYPE=Release
+sudo make install
+
+# Symlink configuration files
+echo "Symlinking configuration files..."
+DOTFILES_DIR=$(PWD)
+# Symlink neovim
+ln -s "$DOTFILES_DIR/nvim" "$HOME/.config/nvim"
+# Symlink tmux
+ln -s "$DOTFILES_DIR/tmux.conf" "$HOME/.tmux.conf"
+echo "Installation and setup complete."
+
--- nvim/init.lua
+++ nvim/init.lua
@@ -230,6 +230,16 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   {
+    'christoomey/vim-tmux-navigator',
+    lazy = false,
+    config = function()
+      vim.keymap.set('n', 'C-h', ':TmuxNavigateLeft<CR>', { desc = 'Window left' })
+      vim.keymap.set('n', 'C-l', ':TmuxNavigateRight<CR>', { desc = 'Window right' })
+      vim.keymap.set('n', 'C-j', ':TmuxNavigateDown<CR>', { desc = 'Window down' })
+      vim.keymap.set('n', 'C-k', ':TmuxNavigateUp<CR>', { desc = 'Window up' })
+    end,
+  },
+  {
     'nvim-tree/nvim-tree.lua',
     version = '*',
     lazy = false,
