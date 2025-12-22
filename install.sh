519a65552537045a5b2cc24c28611634384f5275
becktor <Jonathan@Becktor.org>
2025-12-22 10:20:35 +0100

    desktop

--- install.sh
+++ install.sh
@@ -317,7 +317,7 @@ EOF
 symlink_dotfiles() {
     echo "Symlinking configuration files..."
     DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-    CONFIG_FILES=("nvim" "tmux" "wezterm" "kitty" "ncspot" "hypr" "waybar" "rofi")
+    CONFIG_FILES=("nvim" "tmux" "wezterm" "kitty" "ncspot" "hypr" "waybar" "rofi" "mako")
 
     for dir in "${CONFIG_FILES[@]}"; do
         TARGET="$HOME/.config/$dir"
